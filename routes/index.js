ca1db1930b45c4f853097e610701fa4489e02c6a
Sergio <srosduran@gmail.com>
2020-12-08 17:44:34 +0100

    edit users page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const User = require('../models/User');
 const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.send('home');
 });
+
+router.put('/edit-user', (req, res)=>{
+  const {valueToEdit, userToEdit} = req.body;
+  // console.log([valueToEdit]);
+  User.updateOne({username: req.user.username}, {[valueToEdit]: userToEdit[valueToEdit]})
+    .then((result)=>{
+      res.send(result);
+    })
+    .catch((err)=>{
+      res.send(err);
+    })
+})
 
 module.exports = router;
