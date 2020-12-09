994b46a8825e537a6b59ef976fbae5f3968bae02
Sergio <srosduran@gmail.com>
2020-12-09 16:47:36 +0100

    showing single recipe and working on editing it

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const Recipe = require('../models/Recipe');
-const User = require('../models/User');
+const bcrypt  = require('bcryptjs');
+const Recipe  = require('../models/Recipe');
+const User    = require('../models/User');
 const router  = express.Router();
 
 /* GET home page */
@@ -10,13 +11,38 @@ router.get('/', (req, res, next) => {
 
 router.put('/edit-user', (req, res)=>{
   const {valueToEdit, userToEdit} = req.body;
-  User.updateOne({username: req.user.username}, {[valueToEdit]: userToEdit[valueToEdit]})
-    .then((result)=>{
-      res.send(result);
-    })
-    .catch((err)=>{
-      res.send(err);
-    });
+  if(valueToEdit==='password'){
+    const salt = bcrypt.genSaltSync(10);
+		const hashPass = bcrypt.hashSync(userToEdit[valueToEdit], salt);
+    User.updateOne({username: req.user.username}, {[valueToEdit]: hashPass})
+      .then((result)=>{
+        res.send(result);
+      })
+      .catch((err)=>{
+        res.send(err);
+      });
+  } else {
+    User.updateOne({username: req.user.username}, {[valueToEdit]: userToEdit[valueToEdit]})
+      .then((result)=>{
+        res.send(result);
+      })
+      .catch((err)=>{
+        res.send(err);
+      });
+  }
+});
+
+router.put('/edit-recipe', (req, res)=>{
+  const {valueToEdit, recipeToEdit} = req.body;
+ 
+    Recipe.updateOne({_id: recipeToEdit._id}, {[valueToEdit]: recipeToEdit[valueToEdit]})
+      .then((result)=>{
+        res.send(result);
+      })
+      .catch((err)=>{
+        res.send(err);
+      });
+  
 });
 
 router.post('/addrecipe', (req, res)=>{
@@ -40,6 +66,18 @@ router.get('/alluserrecipes', (req, res)=>{
         res.send(err);
       });
   }
+});
+
+router.get('/recipe/:id', (req, res)=>{
+  const id = req.params.id;
+  Recipe.find({_id: id})
+    .then((result)=>{
+      console.log(result);
+      res.send(result);
+    })
+    .catch((err)=>{
+      res.send(err);
+    })
 });
 
 module.exports = router;
