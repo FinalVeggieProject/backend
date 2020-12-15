5c5286186ee64432b48439c362893d0541ed97a2
Sergio <srosduran@gmail.com>
2020-12-15 09:58:07 +0100

    changed origin on cors

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use((req, res, next) => {
 });
 app.use(cors({
   credentials: true,
-  origin: ["http://localhost:3001"]
+  origin: ["https://veggieplanet.netlify.app/"]
 }));
 app.use((req, res, next)=>{
   res.locals.user = req.user;
